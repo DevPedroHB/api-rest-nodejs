0f28c79dcea65057f710dd0fe55cabb4f2104d91
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-02-06 18:15:20 -0300

    Using cookies on Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { env } from "@/env";
 import { transactionsRoutes } from "@/routes/transactions";
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transactionsRoutes, {
   prefix: "transactions",
 });
